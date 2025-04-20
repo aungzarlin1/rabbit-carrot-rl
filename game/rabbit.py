01761691a95371ae01b4c9f9bcf8fec953122eef
aungzarlin1 <aungzarlin1@gmail.com>
2025-04-19 21:48:10 -0400

    l1

---
+++ game/rabbit.py
@@ -0,0 +1,218 @@
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+import numpy as np
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+Point = namedtuple('Point', 'x, y')
+
+# Colors
+WHITE = (255, 255, 255)
+RED = (200, 0, 0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0, 0, 0)
+
+BLOCK_SIZE = 20
+SPEED = 100
+PROB_WALL = 0.01
+GAME_LEVEL = 1
+MAX_ENEMIES = 2
+
+class RabbitGameAI:
+
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Rabbit Game')
+        self.clock = pygame.time.Clock()
+        self.reset()
+
+        self.rabbit_run_imgs = [
+            pygame.transform.scale(pygame.image.load(f'rabbit_run{i}.png').convert_alpha(), (BLOCK_SIZE, BLOCK_SIZE))
+            for i in range(1, 3)
+        ]
+        self.animation_index = 0
+        self.animation_timer = 0
+        self.animation_delay = 2
+
+        self.carrot_img = pygame.image.load('carrot.png').convert_alpha()
+        self.carrot_img = pygame.transform.scale(self.carrot_img, (BLOCK_SIZE, BLOCK_SIZE))
+
+        self.wall_img = pygame.image.load('wall.png').convert_alpha()
+        self.wall_img = pygame.transform.scale(self.wall_img, (BLOCK_SIZE, BLOCK_SIZE))
+
+        if GAME_LEVEL > 2:
+            self.fox_imgs = [pygame.transform.scale(pygame.image.load(f'fox{i}.png').convert_alpha(), (BLOCK_SIZE, BLOCK_SIZE)) for i in range(1, 3)]
+
+    def reset(self):
+        self.direction = Direction.RIGHT
+        self.head = Point(self.w / 2, self.h / 2)
+        self.rabbit = [self.head]
+        self.score = 0
+        self.wall = self._generate_maze()
+        self.food = None
+        self._place_carrot()
+        self.frame_iteration = 0 
+        self.enemies = []
+        if GAME_LEVEL > 2:
+            for _ in range(MAX_ENEMIES):
+                enemy = Point(random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE,
+                              random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE)
+                enemy_direction = random.choice(list(Direction))
+                self.enemies.append({'position': enemy, 'direction': enemy_direction})
+
+    def _generate_maze(self):
+        maze = [[0 for _ in range(self.w // BLOCK_SIZE)] for _ in range(self.h // BLOCK_SIZE)]
+        for x in range(self.w // BLOCK_SIZE):
+            maze[0][x] = 1
+            maze[self.h // BLOCK_SIZE - 1][x] = 1
+        for y in range(self.h // BLOCK_SIZE):
+            maze[y][0] = 1
+            maze[y][self.w // BLOCK_SIZE - 1] = 1
+        for y in range(1, self.h // BLOCK_SIZE - 1):
+            for x in range(1, self.w // BLOCK_SIZE - 1):
+                if random.random() < PROB_WALL and GAME_LEVEL > 1:
+                    maze[y][x] = 1
+        wall_points = [Point(x * BLOCK_SIZE, y * BLOCK_SIZE) for y in range(self.h // BLOCK_SIZE)
+                       for x in range(self.w // BLOCK_SIZE) if maze[y][x] == 1]
+        return wall_points
+
+    def _place_carrot(self):
+        x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        self.food = Point(x, y)
+        if self.food in self.rabbit or self.food in self.wall:
+            self._place_carrot()
+
+    def play_step(self, action):
+        self.frame_iteration  += 1
+        #1. collect user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # 2. move
+        self._move(action) # update the head
+        self.rabbit.insert(0, self.head)
+        self.rabbit.pop()
+
+
+        # 3. check if game over
+        reward = 0
+        game_over = False
+
+        if self.is_collision() or self.frame_iteration > 100*(self.score+1):
+        # if self.is_collision():
+            game_over = True
+            reward = -10
+            return reward, game_over, self.score
+
+        # 4. place new food or just move
+        if self.head == self.food:
+            self.score += 1
+            reward = 10
+            self._place_carrot()
+        # else:
+            # reward = -1
+        else:
+            reward = -0.1
+        # self.rabbit.pop()
+
+        for enemy in self.enemies:
+            self._move_enemy(enemy)
+
+        self._update_ui()
+        self.clock.tick(SPEED)
+
+        return reward, game_over, self.score
+
+    def is_collision(self, pt=None):
+        if pt is None:
+            pt = self.head
+        if pt.x >= self.w or pt.x < 0 or pt.y >= self.h or pt.y < 0:
+            return True
+        # if pt in self.rabbit[1:]:
+        #     return True
+        if pt in self.wall:
+            return True
+        if pt in [enemy['position'] for enemy in self.enemies]:
+            return True
+        return False
+
+    def _move_enemy(self, enemy):
+        x, y = enemy['position'].x, enemy['position'].y
+        if enemy['direction'] == Direction.RIGHT:
+            x += BLOCK_SIZE
+        elif enemy['direction'] == Direction.LEFT:
+            x -= BLOCK_SIZE
+        elif enemy['direction'] == Direction.DOWN:
+            y += BLOCK_SIZE
+        elif enemy['direction'] == Direction.UP:
+            y -= BLOCK_SIZE
+
+        if self._is_collision(Point(x, y)):
+            enemy['direction'] = random.choice(list(Direction))
+        else:
+            enemy['position'] = Point(x, y)
+
+    def _update_ui(self):
+        self.display.fill(BLACK)
+        self.animation_timer += 1
+        if self.animation_timer >= self.animation_delay:
+            self.animation_timer = 0
+            self.animation_index = (self.animation_index + 1) % len(self.rabbit_run_imgs)
+
+        for pt in self.rabbit:
+            frame = self.rabbit_run_imgs[self.animation_index]
+            self.display.blit(frame, (pt.x, pt.y))
+
+        for w in self.wall:
+            self.display.blit(self.wall_img, (w.x, w.y))
+
+        self.display.blit(self.carrot_img, (self.food.x, self.food.y))
+
+        for enemy in self.enemies:
+            fox_frame = self.fox_imgs[self.animation_index % 2]
+            self.display.blit(fox_frame, (enemy['position'].x, enemy['position'].y))
+
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [10, 10])
+        pygame.display.flip()
+
+    def _move(self, action):
+        clockwise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        idx = clockwise.index(self.direction)
+
+        if np.array_equal(action, [1, 0, 0]):
+            new_dir = clockwise[idx]
+        elif np.array_equal(action, [0, 1, 0]):
+            new_dir = clockwise[(idx + 1) % 4]
+        else:
+            new_dir = clockwise[(idx - 1) % 4]
+
+        self.direction = new_dir
+
+        x = self.head.x
+        y = self.head.y
+        if self.direction == Direction.RIGHT:
+            x += BLOCK_SIZE
+        elif self.direction == Direction.LEFT:
+            x -= BLOCK_SIZE
+        elif self.direction == Direction.DOWN:
+            y += BLOCK_SIZE
+        elif self.direction == Direction.UP:
+            y -= BLOCK_SIZE
+
+        self.head = Point(x, y)
---
+++ test.py
@@ -0,0 +1,136 @@
+import torch
+import pygame
+import random
+import numpy as np
+from collections import deque
+# from game import SnakeGameAI, Direction, Point
+from game.rabbit import RabbitGameAI, Direction, Point
+from model.model import Linear_QNet, QTrainer
+from utils.helper import plot
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+
+    def get_state(self, game):
+        head = game.rabbit[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+        
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)),
+
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
+            (dir_l and game.is_collision(point_d)),
+            
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            # Food location 
+            game.food.x < game.head.x,  # food left
+            game.food.x > game.head.x,  # food right
+            game.food.y < game.head.y,  # food up
+            game.food.y > game.head.y  # food down
+            ]
+
+        return np.array(state, dtype=int)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
+
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+        #for state, action, reward, nexrt_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def get_action(self, state):
+        # random moves: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games
+        final_move = [0,0,0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+
+def test():
+    agent = Agent()
+    # agent.model.load()  # Assuming you have the model save/load method defined
+    agent.model.load_state_dict(torch.load("results/model.pth"))
+# model.eval()
+    game = RabbitGameAI()
+    total_score = 0
+    while True:
+        state_old = agent.get_state(game)
+
+        # Get action from the trained model (no random exploration)
+        final_move = agent.get_action(state_old)
+
+        # Perform the move and get the new state
+        reward, done, score = game.play_step(final_move)
+        
+        # Add the score
+        total_score += score
+
+        if done:
+            print(f"Game Over! Final Score: {score}")
+            game.reset()
+
+        # Optionally, you can render the game state to visually track the performance
+        game._update_ui()
+        pygame.display.flip()
+
+        if done:
+            break
+
+    print(f"Total Score after testing: {total_score}")
+
+if __name__ == "__main__":
+    test()
